6a66bf7c3eefad7e959401411402f8dcf34043b4
hayaboy <hayaboy@korea.ac.kr>
2022-11-08 12:25:50 +0900

    11/8 12시 25분

---
+++ src/main/java/com/himedia/hello/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.himedia.hello.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/fsdajkslsdfzjk")
+    public String gsefgeg(){
+        return "Hi";
+    }
+}
